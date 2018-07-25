cab631340e34ca7c498fa33dcbd931603666b325
Andrew Helmer <ahelmer7@live.com>
2018-07-25 12:18:28 -0700

    client server work

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const express = require('express');
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 const cors = require('cors');
 
-const PORT = process.env.PORT; //this is the preferred method
-// const PORT = process.env.PORT || 3000; //this is when running locally
+// const PORT = process.env.PORT; //this is the preferred method
+const PORT = process.env.PORT || 3000; //this is when running locally
 
 const app = express();
 app.use(cors());
@@ -19,11 +19,11 @@ app.use(cors());
 // const conString = 'postgres://hoffit:password@localhost:5432/books_app';
 // This next line is more standard for deployed apps where an environment variable is used to detect whether running
 // local or on a deployed server. It's set in index.js.
-const conString = 'process.env.DATABASE_URL';
+const conString = process.env.DATABASE_URL;
 // This next line is incorrect...don't hard config database url...
 // const conString = 'postgres://sokqhqjxzbhshd:59a24d75cd73b92f8cb8cd9dd2bcb86e5d32ba08f22992a18fc1487b96c16b2d@ec2-54-204-23-228.compute-1.amazonaws.com:5432/d3t5euqm6musdv'
-// When running local, extra step to setup that env variable using terminal command export DATABASE_URL='postgres://localhost:5432'
-
+// When running local, extra step to setup that env variable using terminal command export DATABASE_URL='postgres://hoffit:password@localhost:5432/books_app'
+//                                                                                         DATABASE_URL='postgres://postgres:Angrybritches07@localhost:5432/books_app'
 console.log('app server side con string is '+conString);
 
 const client = new pg.Client(conString);
@@ -50,3 +50,4 @@ app.get('/books', (request, response) => {
       console.error(err);
     });
 });
+app.get('*', (request, result) => result.status(404).send('this route does not exist'));
